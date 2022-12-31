4b777e13640c632a3ae6a74b44e615ee1c19713f
mucahitkarakus <mocococo.80@gmail.com>
2022-12-31 13:38:43 +0300

    portfolio components done

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,7 +1,63 @@
+import Image from "next/image";
 import React from "react";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      <h1>Travel Photos</h1>
+      <div>
+        <div>
+          <Image
+            src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+            alt="/"
+            layout="responsive"
+            width="677"
+            height="451"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1540390769625-2fc3f8b1d50c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80"
+            alt="/"
+            width="215"
+            height="217"
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1472791108553-c9405341e398?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2437&q=80"
+            alt="/"
+            width="215"
+            height="217"
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+            alt="/"
+            width="215"
+            height="217"
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="https://images.unsplash.com/photo-1469362102473-8622cfb973cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2419&q=80"
+            alt="/"
+            width="215"
+            height="217"
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
