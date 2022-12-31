4bef5161c7e7f7d38f61fc4b1cd79abf597e2c7e
mucahitkarakus <mocococo.80@gmail.com>
2022-12-31 12:52:07 +0300

    slider onclick

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,21 +1,51 @@
 import Image from "next/image";
-import React from "react";
+import React, {useState} from "react";
 import {SliderData} from "./SliderData";
+import {FaArrowCircleLeft, FaArrowCircleRight} from "react-icons/fa";
 
 const Slider = ({slides}) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
   return (
     <div id="gallery">
       <h1>Gallery</h1>
       <div>
         {SliderData.map((slide, index) => {
           return (
-            <Image
-              src={slide.image}
-              alt="/"
-              width="1440"
-              height="600"
-              objectFit="cover"
-            />
+            <div
+              key={index}
+              className={
+                index === current
+                  ? "opacity-[1] ease-in duration-1000"
+                  : "opacity-0"
+              }
+            >
+              <div className="relative flex justify-center p-4">
+                <FaArrowCircleLeft
+                  onClick={prevSlide}
+                  className="absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]"
+                  size={50}
+                />
+                {index === current && (
+                  <Image src={slide.image} alt="/" width="1440" height="600" />
+                )}
+                <FaArrowCircleRight
+                  onClick={nextSlide}
+                  className="absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]"
+                  size={50}
+                />
+              </div>
+            </div>
           );
         })}
       </div>
