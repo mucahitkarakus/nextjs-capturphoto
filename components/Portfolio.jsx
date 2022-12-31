d9b4851c5e844219650813926ebfb42f63626789
mucahitkarakus <mocococo.80@gmail.com>
2022-12-31 13:40:03 +0300

    portfolio responsive done

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    <div>
-      <h1>Travel Photos</h1>
-      <div>
-        <div>
+    <div className="max-w-[1240px] mx-auto py-16 text-center">
+      <h1 className="font-bold text-2xl p-4">Travel Photos</h1>
+      <div className="grid grid-rows-none md:grid-cols-5 p-4 gap-4">
+        <div className="w-full h-full col-span-2 md:col-span-3 row-span-2">
           <Image
             src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
             alt="/"
