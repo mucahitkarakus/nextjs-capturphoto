88bf38b23ff0ff8895203c81c85001c52d64f5c3
mucahitkarakus <mocococo.80@gmail.com>
2022-12-31 13:29:36 +0300

    work and contact page create

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
             <Link href="/#gallery">Gallery</Link>
           </li>
           <li className="p-4">
-            <Link href="/portfolio">Work</Link>
+            <Link href="/work">Work</Link>
           </li>
           <li className="p-4">
             <Link href="/contact">Contact</Link>
@@ -73,7 +73,7 @@ const Navbar = () => {
               <Link href="/#gallery">Gallery</Link>
             </li>
             <li className="p-4 text-4xl hover:text-gray-500">
-              <Link href="/portfolio">Work</Link>
+              <Link href="/work">Work</Link>
             </li>
             <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/contact">Contact</Link>
---
+++ pages/work.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const work = () => {
+  return (
+    <div>
+      <Hero
+        heading="My Work "
+        message="This is some of my recent work traveling the worl"
+      />
+    </div>
+  );
+};
+
+export default work;
